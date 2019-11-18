6b621ca9cf1c2c9166e23a19392d28cd506862d8
alexandru.rolea <alexandru.rolea@softvision.ro>
2019-11-18 12:14:54 +0200

    Refactored database init

--- reactive-persistence/src/main/java/com/rolea/learning/reactive/persistence/config/PersistenceConfig.java
+++ reactive-persistence/src/main/java/com/rolea/learning/reactive/persistence/config/PersistenceConfig.java
@@ -1,16 +1,14 @@
 package com.rolea.learning.reactive.persistence.config;
 
-import com.rolea.learning.reactive.server.model.Student;
 import io.r2dbc.h2.H2ConnectionConfiguration;
 import io.r2dbc.h2.H2ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
-import org.springframework.data.r2dbc.function.DatabaseClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import reactor.core.publisher.Mono;
-
-import static java.util.Arrays.asList;
+import reactor.core.publisher.Flux;
 
 @Configuration
 @EnableR2dbcRepositories(basePackages = {"com.rolea.learning.reactive.persistence.repositories"})
@@ -18,30 +16,30 @@ public class PersistenceConfig extends AbstractR2dbcConfiguration {
 
 	@Bean
 	public H2ConnectionFactory connectionFactory() {
-		H2ConnectionFactory cf = new H2ConnectionFactory(
+		return new H2ConnectionFactory(
 				H2ConnectionConfiguration.builder()
 						.url("mem:testdb;DB_CLOSE_DELAY=-1;")
 						.username("sa")
 						.build()
 		);
+	}
 
-		DatabaseClient client = DatabaseClient.create(cf);
-		client.execute().sql("CREATE TABLE STUDENT(id BIGINT PRIMARY KEY, name VARCHAR(255))").fetch()
-				.rowsUpdated()
-				.as(Mono::block);
-
-		asList(
-				Student.builder().id(1L).name("Foo").build(),
-				Student.builder().id(2L).name("Bar").build(),
-				Student.builder().id(3L).name("Baz").build()
-		).forEach(student -> client.insert()
-				.into(Student.class)
-				.using(student)
-				.then()
-				.as(Mono::block)
-		);
-
-		return cf;
+	@Bean
+	public CommandLineRunner initDatabase(ConnectionFactory cf) {
+		return (args) ->
+				Flux.from(cf.create())
+						.flatMap(c ->
+								Flux.from(c.createBatch()
+										.add("drop table if exists STUDENT")
+										.add("create table STUDENT(id BIGINT PRIMARY KEY, name VARCHAR(255))")
+										.add("insert into STUDENT(id,name) values(1,'Foo')")
+										.add("insert into STUDENT(id,name) values(2,'Bar')")
+										.add("insert into STUDENT(id,name) values(3,'Baz')")
+										.execute())
+										.doFinally((st) -> c.close())
+						)
+						.log()
+						.blockLast();
 	}
 
 }
